b3f8a140c5fa3b69f516940def62b7ed15ac5453
tamaina <tamaina@hotmail.co.jp>
2025-08-01 01:00:35 +0900

    Revert "wip"
    This reverts commit a3315b4711ab1dff054e3736938ba400ee1dd7f2.

--- update.ubuntu.sh
+++ update.ubuntu.sh
@@ -85,10 +85,6 @@ systemctl stop "$host"
 #region work with misskey user
 su "$misskey_user" << MKEOF
 set -eu;
-
-source ~/.nvm/nvm.sh;
-nvm install 22;
-
 cd ~/$misskey_directory;
 
 tput setaf 3;
